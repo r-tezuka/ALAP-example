af397f893708ef8b94e45c76c5d42e89be1b75ec
r-tezuka <>
2022-10-23 20:33:00 +0900

    add animation

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from importlib import import_module
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
 
-n = 8
-r = 10
-handles = [2, 3, 6]
+
+def rand_ints_nodup(a, b, k):
+    ns = []
+    while len(ns) < k:
+        n = random.randint(a, b)
+        if not n in ns:
+            ns.append(n)
+    return ns
+
+
+n = 16
+r = 1
+handles = rand_ints_nodup(0, n-1, 4)
+print(handles)
 
 
 def get_ls(xs, ys):
     return np.array([np.sqrt((xs[i] - xs[i-1])**2 + (ys[i] - ys[i-1]) ** 2)
-                     for i in range(len(xs)) if i != 0])
+                     for i in range(len(xs))])
 
 
 def get_ns(xs, ys):
     ls = get_ls(xs, ys)
-    nxs = np.array([(ys[i] - ys[i-1]) / ls[i-1]
-                   for i in range(len(xs)) if i != 0])
-    nys = np.array([(-xs[i] + xs[i-1]) / ls[i-1]
-                   for i in range(len(xs)) if i != 0])
+    nxs = np.array([(ys[i] - ys[i-1]) / ls[i]
+                   for i in range(len(xs))])
+    nys = np.array([(-xs[i] + xs[i-1]) / ls[i]
+                   for i in range(len(xs))])
     return nxs, nys
 
 
@@ -28,46 +40,50 @@ def get_rad(u, v):
     return np.arccos(np.clip(c, -1.0, 1.0))
 
 
-def opt_v(xs, ys, ls, xs_0, ys_0, ls_0):
-    l_avg = sum(ls_0) / len(ls_0)
-    nxs, nys = get_ns(xs, ys)
+def opt_v(xs, ys, ls, xs_0, ys_0, ls_0, xs_h, ys_h):
+    l_avg = sum(ls) / len(ls)
+    nxs, nys = get_ns(xs_0, ys_0)
 
-    a_normal = np.array([np.zeros(2 * (n + 1)) for _ in range(2 * n)])
-    b_normal = np.zeros(2 * n)
-    a_edge = np.array([np.zeros(2 * (n + 1)) for _ in range(2 * n)])
+    a_normal = np.array([np.zeros(2 * n) for _ in range(n)])
+    b_normal = np.zeros(n)
+    a_edge = np.array([np.zeros(2 * n) for _ in range(2 * n)])
     b_edge = np.zeros(2 * n)
 
     for i in range(n):
-        j = i + 1
-        c = max(1, ls[i]/l_avg)
+        j = i - 1
+        if j < 0:
+            j = n - 1
+        c = max([1, ls[i]/l_avg])
 
         # set x
-        a_normal[i][i] = nxs[i] * c / ls[i]
+        a_normal[i][i] = c * nxs[i] / ls[i]
         a_normal[i][j] = -c * nxs[i] / ls[i]
         a_edge[i][i] = c / ls[i]
         a_edge[i][j] = -c / ls[i]
-        b_edge[i] = -c / ls_0[i] * (xs_0[j] - xs_0[i])
+        b_edge[i] = c / ls_0[i] * (xs_0[i] - xs_0[j])
 
         # set y
-        a_normal[n + i][n + 1 + i] = nys[i] * c / ls[i]
-        a_normal[n + i][n + 1 + j] = -c * nys[i] / ls[i]
-        a_edge[n + i][n + 1 + i] = c / ls[i]
-        a_edge[n + i][n + 1 + j] = -c / ls[i]
-        b_edge[n + i] = -c / ls_0[i] * (ys_0[j] - ys_0[i])
+        a_normal[i][n + i] = c * nys[i] / ls[i]
+        a_normal[i][n + j] = -c * nys[i] / ls[i]
+        a_edge[n + i][n + i] = c / ls[i]
+        a_edge[n + i][n + j] = -c / ls[i]
+        b_edge[n + i] = c / ls_0[i] * (ys_0[i] - ys_0[j])
 
     wc = 100000
-    a_handles = [[wc if (i in handles or i-n-1 in handles) and i ==
-                  j else 0 for i in range(2 * (n+1))] for j in range(2 * (n+1))]
-    px = xs_0
-    py = ys_0
-    b_handles = np.zeros(2 * (n + 1))
-    for i in range(n+1):
+    a_handles = [[wc if (i in handles or i-n in handles) and i ==
+                  j else 0 for i in range(2 * n)] for j in range(2 * n)]
+
+    b_handles = np.zeros(2 * n)
+    for i in range(n):
         if i in handles:
-            b_handles[i] = wc * px[i]
-            b_handles[n+1+i] = wc * py[i]
+            b_handles[i] = wc * xs_h[i]
+            b_handles[n+i] = wc * ys_h[i]
 
     A_v = np.concatenate([a_normal, a_edge, a_handles])
     b_v = np.concatenate([b_normal, b_edge, b_handles])
+    A_v = np.concatenate([a_normal, a_edge, a_handles])
+    b_v = np.concatenate([b_normal, b_edge, b_handles])
+
     X_v = np.linalg.solve(np.dot(A_v.T, A_v), np.dot(A_v.T, b_v))
     pn = int(len(X_v) / 2)
     xs = X_v[:pn]
@@ -76,11 +92,11 @@ def opt_v(xs, ys, ls, xs_0, ys_0, ls_0):
 
 
 def opt_l(xs, ys):
-    ls_0 = get_ls(xs, ys)
-    l_avg = sum(ls_0) / len(ls_0)
+    ls = get_ls(xs, ys)
+    l_avg = sum(ls) / len(ls)
     a_linearized = [
         [1/np.sqrt(l_avg) if i == j else 0 for i in range(n)] for j in range(n)]
-    b_linearized = [l / np.sqrt(l_avg) for l in ls_0]
+    b_linearized = [l / np.sqrt(l_avg) for l in ls]
     a_tangent = np.array([np.zeros(n) for _ in range(n)])
     b_tangent = np.zeros(n)
 
@@ -94,8 +110,8 @@ def opt_l(xs, ys):
             wt = np.exp(-(rad - np.pi) ** 2 / (2 * (np.pi / 6) ** 2))
         else:
             wt = 0.001
-        r = ls_0[i] / ls_0[i-1]
-        a = np.sqrt(wt * l_avg / ls_0[i])
+        r = ls[i] / ls[k]
+        a = np.sqrt(wt * l_avg / ls[i])
         a_tangent[i][i] = a
         a_tangent[i][j] = -a * r
 
@@ -105,14 +121,50 @@ def opt_l(xs, ys):
     return X_l
 
 
-xs_0 = np.array([r * np.cos(2 * np.pi * x / n) for x in range(n+1)])
-ys_0 = np.array([r * np.sin(2 * np.pi * x / n) for x in range(n+1)])
+xs_0 = np.array([r * np.cos(2 * np.pi * x / n) for x in range(n)])
+ys_0 = np.array([r * np.sin(2 * np.pi * x / n) for x in range(n)])
 ls_0 = get_ls(xs_0, ys_0)
+
+ims = []
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 3, 1)
+ax2 = fig.add_subplot(1, 3, 2)
+ax3 = fig.add_subplot(1, 3, 3)
+plt_xs = np.append(xs_0, xs_0[0])
+plt_ys = np.append(ys_0, ys_0[0])
+im1 = ax1.plot(plt_xs, plt_ys, c='k')
+plt_xs_h = [x for i, x in enumerate(xs_0) if i in handles]
+plt_ys_h = [y for i, y in enumerate(ys_0) if i in handles]
+ax1.scatter(plt_xs_h, plt_ys_h, c='r')
+ax1.axis('square')
+
+xs_h = [random.uniform(0, 2) * r * np.cos(2 * np.pi * i / n)
+        if i in handles else xs_0[i] for i in range(n)]
+ys_h = [random.uniform(0, 2) * r * np.sin(2 * np.pi * i / n)
+        if i in handles else ys_0[i] for i in range(n)]
+
+plt_xs = np.append(xs_h, xs_h[0])
+plt_ys = np.append(ys_h, ys_h[0])
+im3 = ax3.plot(plt_xs, plt_ys, c='k')
+plt_xs_h = [x for i, x in enumerate(xs_h) if i in handles]
+plt_ys_h = [y for i, y in enumerate(ys_h) if i in handles]
+ax3.scatter(plt_xs_h, plt_ys_h, c='r')
+ax3.axis('square')
+
 xs, ys = xs_0, ys_0
 ls = ls_0
-for _ in range(10):
-    xs, ys = opt_v(xs, ys, ls, xs_0, ys_0, ls_0)
+
+
+for _ in range(20):
+    xs, ys = opt_v(xs, ys, ls, xs_0, ys_0, ls_0, xs_h, ys_h)
     ls = opt_l(xs, ys)
-plt.plot(xs, ys)
-plt.axis('square')
+    plt_xs = np.append(xs, xs[0])
+    plt_ys = np.append(ys, ys[0])
+    im2 = ax2.plot(plt_xs, plt_ys, c='k')
+    plt_xs_h = [x for i, x in enumerate(xs) if i in handles]
+    plt_ys_h = [y for i, y in enumerate(ys) if i in handles]
+    ax2.scatter(plt_xs_h, plt_ys_h, c='r')
+    ax2.axis('square')
+    ims.append(im1+im2+im3)
+ani = animation.ArtistAnimation(fig, ims, interval=100)
 plt.show()
